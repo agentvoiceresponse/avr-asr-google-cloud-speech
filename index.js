1c25aad66a3f6dbfc12bb2aa7d0e31ff15cec072
Giuseppe Careri <giuseppe.careri@ingogroup.it>
2024-10-07 22:43:07 +0200

    Added docker build image

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const speechClient = new SpeechClient();
 
 const requestConfig = {
   config: {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 8000, // Adjust as needed
+    encoding: 'LINEAR16', // Asterisk Audio Socket Encoding
+    sampleRateHertz: 8000, // Asterisk Audio Socket Sample Rate Hertz
     languageCode: process.env.SPEECH_RECOGNITION_LANGUAGE || 'en-US',
     model: process.env.SPEECH_RECOGNITION_MODEL || 'telephony'
   },
@@ -19,6 +19,7 @@ const requestConfig = {
 };
 
 console.log("Google Speech Configuration", requestConfig)
+
 /**
  * Class representing a writable stream for audio data.
  * @extends Writable
@@ -34,54 +35,90 @@ class AudioWritableStream extends Writable {
   }
 
   _write(chunk, encoding, callback) {
-    this.recognizeStream.write(chunk);
-    callback();
+    try {
+      this.recognizeStream.write(chunk);
+      callback()
+    } catch (error) {
+      console.error(error)
+      callback(error);
+    }
   }
 
   _final(callback) {
-    this.recognizeStream.end();
-    callback();
+    try {
+      this.recognizeStream.end();
+      callback();
+    } catch (error) {
+      console.error(error)
+      callback(error);
+    }
   }
 }
 
-app.post('/audio-stream', (req, res) => {
-  const recognizeStream = speechClient
-    .streamingRecognize(requestConfig)
-    .on('error', (err) => {
-      console.error('Google Speech API Error:', err);
-      res.status(500).send('Error with Speech API');
-    })
-    .on('data', (data) => {
-      if (data.results[0] && data.results[0].alternatives[0]) {
-        const transcript = data.results[0].alternatives[0].transcript;
-        console.log(`Transcription: ${transcript}`);
-        if (data.results[0].alternatives[0].confidence) {
-            console.log(`Confidence: ${data.results[0].alternatives[0].confidence}`);
-            res.write(`${transcript}\n\n`); // Stream the transcript to the client
+/**
+ * Handles an audio stream from the client and uses the Google Cloud Speech
+ * to Speech API to recognize the speech and stream the transcript back to the
+ * client.
+ *
+ * @param {Object} req - The Express request object
+ * @param {Object} res - The Express response object
+ */
+const handleAudioStream = (req, res) => {
+  try {
+    const recognizeStream = speechClient
+      .streamingRecognize(requestConfig)
+      .on('error', (err) => {
+        console.error('Google Speech API Error:', err);
+        res.status(500).send('Error with Speech API');
+      })
+      .on('data', (data) => {
+        if (data.results[0] && data.results[0].alternatives[0]) {
+          const transcript = data.results[0].alternatives[0].transcript;
+          const confidance = data.results[0].alternatives[0].confidence;
+          if (transcript) {
+            console.log(`${transcript}...`);
+            if (confidance) {
+              console.log(`Transcript: ${transcript}`);
+              console.log(`Confidence: ${confidance}`);
+              res.write(transcript); // Stream the transcript to the client
+            }
+          }
         }
+      })
+      .on('end', () => {
+        res.end(); // Close the connection when transcription ends
+      });
+
+    // Set appropriate headers for streaming response
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const audioWritableStream = new AudioWritableStream(recognizeStream);
+    req.pipe(audioWritableStream);
+
+    req.on('end', () => {
+      try {
+        audioWritableStream.end();
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error receiving audio stream' });
       }
-    })
-    .on('end', () => {
-      res.end(); // Close the connection when transcription ends
     });
 
-  // Set appropriate headers for streaming response
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-  
-  const audioWritableStream = new AudioWritableStream(recognizeStream);
-  req.pipe(audioWritableStream);
-
-  req.on('end', () => {
-    audioWritableStream.end();
-  });
-
-  req.on('error', (err) => {
+    req.on('error', (err) => {
+      console.error('Error receiving audio stream:', err);
+      res.status(500).json({ message: 'Error receiving audio stream' });
+    });
+  } catch (err) {
     console.error('Error receiving audio stream:', err);
-    res.status(500).json({ message: 'Error receiving audio stream' });
-  });
-});
+    res.status(500).json({ message: err.message });
+
+  }
+
+}
+
+app.post('/speech-to-text-stream', handleAudioStream);
 
 const port = process.env.PORT || 6001;
 app.listen(port, () => {
